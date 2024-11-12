1cb333cd6d1ad00c09f752956575e82387cb8057
Romain Fafet <farom57@gmail.com>
2024-11-12 20:22:35 +0100

    fix test

--- esphome/components/hitachi_hlink/hitachi_hlink.h
+++ esphome/components/hitachi_hlink/hitachi_hlink.h
@@ -4,7 +4,7 @@
 #include "esphome/components/uart/uart.h"
 #include "esphome/components/climate/climate.h"
 
-#include "esphome/components/sensor/sensor.h"
+//#include "esphome/components/sensor/sensor.h"
 #include "esphome/components/binary_sensor/binary_sensor.h"
 
 namespace esphome {
